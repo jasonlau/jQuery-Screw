805cb8e36c64fb59df6d0740a8bef62dfef9cd58
Jason Lau <jason_lau@live.com>
2011-05-29 12:53:56 -0500

    Version 1.0.1 improves transition between loadingHTML and loaded content. Aslo includes a bug fix or two. Additionally, there are new examples added to the repository.

--- jquery.screw.js
+++ jquery.screw.js
@@ -2,7 +2,7 @@
 /* 
     Screw - A jQuery plugin
     ==================================================================
-    ©2010 JasonLau.biz - Version 1.0.0
+    ©2010-2011 JasonLau.biz - Version 1.0.1
     ==================================================================
     This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
@@ -34,82 +34,126 @@
               });
               
               var screwIt = function(it){
-                var h = $(window).height(), st = it.scrollTop();
-                $(".screw-image").each(function(){
-                    var pos = $(this).offset(), t = h+st;
+                var h = $(window).height(), st = it.scrollTop(), t = h+st;
+                $(".screw-image").each(function(){                   
+                    var pos = $(this).offset(), rand = Math.round(Math.random()*1000);
                     if(t >= pos.top){
                     if(!$(this).hasClass('screw-loaded')){
-                        $(this).html('<img src="' + $(this).attr('rel') + '" />');
-                        $(this).addClass('screw-loaded');
+                        
+                        $(this).html('<div id="screw-loading-' + rand + '">' + option.loadingHTML + '</div>');
+                        // Stop cache
+                        var url = $(this).attr('rel'), patt = /&/g;
+                        if(patt.test(url)){
+                            url += '&screw_rand=' + rand;
+                        } else {
+                            url += '?screw_rand=' + rand;
+                        }
+                        // Preload image
+                        objImage = new Image();
+                        objImage.src = url;
+                        var o = $(this);
+                        objImage.onload = function(){
+                            o.append('<img style="display:none" id="screw-content-' + rand + '" class="screw-content" src="' + url + '" />');                            
+                            $('#screw-loading-' + rand).fadeOut('slow', function(){
+                                $('#screw-content-' + rand).fadeIn('slow');
+                                o.addClass('screw-loaded');
+                            });
+                        };
                     }                        
                     }
                 });	
                 
                 $(".screw").each(function(){
-                    var pos = $(this).offset(), t = h+st;
+                    var pos = $(this).offset(), rand = Math.round(Math.random()*1000);
                     if(t >= pos.top){
                     if(!$(this).hasClass('screw-loaded') || $(this).hasClass('screw-repeat')){
                         var o = $(this);
+                        if(option.loadingHTML){
+                            o.html('<div id="screw-loading-' + rand + '">' + option.loadingHTML + '</div>');
+                        }
+                        
                         if(o.hasClass('screw-append')){
                         if($(this).attr('rel')){
-                          $.get($(this).attr('rel'), function(data) {
-                             o.append(data);
+                          $.get($(this).attr('rel'), { screwrand : Math.round(Math.random()*1000) }, function(data) {
+                             o.append('<div style="display:none" id="screw-content-' + rand + '" class="screw-content">' + data + '</div>');
+                             showContent(rand);
                         });  
                         } else if($(this).attr('rev')){
-                            o.append($(this).attr('rev'));
+                            o.append('<div style="display:none" id="screw-content-' + rand + '" class="screw-content">' + $(this).attr('rev') + '</div>');
+                            showContent(rand);
                         } 
                         } else if(o.hasClass('screw-prepend')){
                         if($(this).attr('rel')){
-                          $.get($(this).attr('rel'), function(data) {
-                             o.prepend(data);
+                          $.get($(this).attr('rel'), { screwrand : Math.round(Math.random()*1000) }, function(data) {
+                             o.prepend('<div style="display:none" id="screw-content-' + rand + '" class="screw-content">' + data + '</div>');
+                             showContent(rand);
                         });  
                         } else if($(this).attr('rev')){
-                            o.prepend($(this).attr('rev'));
+                            o.prepend('<div style="display:none" id="screw-content-' + rand + '" class="screw-content">' + $(this).attr('rev') + '</div>');
+                            showContent(rand);
                         } 
                         } else if(o.hasClass('screw-before')){
                         if($(this).attr('rel')){
-                          $.get($(this).attr('rel'), function(data) {
-                             o.before(data);
+                          $.get($(this).attr('rel'), { screwrand : Math.round(Math.random()*1000) }, function(data) {
+                             o.before('<div style="display:none" id="screw-content-' + rand + '" class="screw-content">' + data + '</div>');
+                             showContent(rand);
                         });  
                         } else if($(this).attr('rev')){
-                            o.before($(this).attr('rev'));
-                        } 
-                        } else if(o.hasClass('screw-after')){
-                        if($(this).attr('rel')){
-                          $.get($(this).attr('rel'), function(data) {
-                             o.after(data);
-                        });  
-                        } else if($(this).attr('rev')){
-                            o.after($(this).attr('rev'));
-                        } 
-                        } else {
-                           o.html(option.loadingHTML);
-                        if($(this).attr('rel')){
-                          $.get($(this).attr('rel'), function(data) {
-                             o.html(data);
-                        });  
-                        } else if($(this).attr('rev')){
-                            o.html($(this).attr('rev'));
-                        } 
+                            o.before('<div style="display:none" id="screw-content-' + rand + '" class="screw-content">' + $(this).attr('rev') + '</div>');
+                            showContent(rand);
                         }
-                        o.html(option.loadingHTML);
-                        if($(this).attr('rel')){
-                          $.get($(this).attr('rel'), function(data) {
-                             o.html(data);
-                        });  
-                        } else if($(this).attr('rev')){
-                            o.html($(this).attr('rev'));
+                        
+                        if(o.hasClass('screw-repeat') && pos.top < $(window).height()){
+                            if($(this).attr('rel')){
+                            $.get($(this).attr('rel'), { screwrand : Math.round(Math.random()*1000) }, function(data) {
+                                    o.before('<div style="display:none" id="screw-content-' + rand + '" class="screw-content">' + data + '</div>');
+                                    showContent(rand);
+                            });
+                            } else if($(this).attr('rev')){
+                                o.before('<div style="display:none" id="screw-content-' + rand + '" class="screw-content">' + $(this).attr('rev') + '</div>');
+                                showContent(rand);
+                            }
                         }
                          
-                        $(this).addClass('screw-loaded');                     
+                        } else if(o.hasClass('screw-after')){
+                        if($(this).attr('rel')){
+                          $.get($(this).attr('rel'), { screwrand : Math.round(Math.random()*1000) }, function(data) {
+                             o.after('<div style="display:none" id="screw-content-' + rand + '" class="screw-content">' + data + '</div>');
+                             showContent(rand);
+                        });  
+                        } else if($(this).attr('rev')){
+                            o.after('<div style="display:none" id="screw-content-' + rand + '" class="screw-content">' + $(this).attr('rev') + '</div>');
+                            showContent(rand);
+                        } 
+                        } else {
+                        if($(this).attr('rel')){
+                          $.get($(this).attr('rel'), { screwrand : Math.round(Math.random()*1000) }, function(data) {
+                             o.append('<div style="display:none" id="screw-content-' + rand + '" class="screw-content">' + data + '</div>');
+                             showContent(rand);
+                        });  
+                        } else if($(this).attr('rev')){
+                            o.append('<div style="display:none" id="screw-content-' + rand + '" class="screw-content">' + $(this).attr('rev') + '</div>');
+                            showContent(rand);
+                        } 
+                        }
+                        o.addClass('screw-loaded');                                           
                     }                        
                     }
                 });
               };
               
-              screwIt($(window));
+              var showContent = function(rand){
+                if(option.loadingHTML){
+                    $('#screw-loading-' + rand).fadeOut('slow', function(){
+                        $('#screw-content-' + rand).fadeIn('slow');
+                    });
+                } else {
+                    $('#screw-content-' + rand).fadeIn('slow');
+                }               
+              };
               
-            });
+              screwIt($(window));
+    		});
     	}
 	});
 	
