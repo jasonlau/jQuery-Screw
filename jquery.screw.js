3f4dceae7239f1eefc7d408ce494278f51aa1f0b
Jason Lau <jason_lau@live.com>
2012-03-10 21:01:51 -0600

    Made some overdue changes to this plugin. Screw now supports vertical AND horizontal scroll detection. Also, Screw can now be called on any scrollable object, so you are no longer limited to calling Screw on the body selector only. You can now call Screw on any scrollable object, like a div.

--- jquery.screw.js
+++ jquery.screw.js
@@ -2,7 +2,7 @@
 /* 
     Screw - A jQuery plugin
     ==================================================================
-    ©2010-2011 JasonLau.biz - Version 1.0.4
+    ©2010-2012 JasonLau.biz - Version 1.0.5
     ==================================================================
     This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
@@ -24,39 +24,104 @@
  		screw: function(options) {
 			var defaults = {
 			 loadingHTML : 'Loading ... '
-			}				
-			var option =  $.extend(defaults, options);
+			},
+            option =  $.extend(defaults, options); 
             var obj = $(this);
-
-    		return this.each(function() {   		  
-              $(window).scroll(function() {
-                 screwIt($(this));                             
-              });
-              
-              var screwIt = function(it){
-                var h = $(window).height(), w = $(window).width(), st = it.scrollTop(), t = h+st;
-                $(".screw-image").each(function(){                   
-                    var pos = $(this).offset(), rand = Math.round(Math.random()*1000);
-                    if(t >= pos.top && pos.left <= w){
-                    if(!$(this).hasClass('screw-loaded') && !$(this).hasClass('screw-loading')){
+            return this.each(function() {              
+               var is_viewable = function(mother, baby){
+                var x1 = mother.offset().left,
+                    y1 = mother.offset().top,
+                    w = mother.outerWidth(),
+                    h = mother.outerHeight();
+                var xp1 = baby.offset().left,
+                    yp1 = baby.offset().top,
+                    wp = baby.outerWidth(),
+                    hp = baby.outerHeight();
+                var x2 = x1+w,
+                    y2 = y1+h,
+                    xp2 = xp1+wp,
+                    yp2 = yp1+hp;
+                if(xp1 >= x1 && xp1 <= x2){
+                    if(yp1 >= y1 && yp1 <= y2){
+                        return true;
+                    } else if(y1>=yp1 && y1<=yp2){
+                        return true;
+                    }
+                } else if(x1>=xp1 && x1<=xp2){
+                    if(yp1>=y1 && yp1<=y2){
+                        return true;
+                    } else if(y1>=yp1 && y1<=yp2){
+                        return true;
+                    }
+                }
+                return false;
+               }; // is_viewable
+               
+               var screwIt = function(){
+                
+                if(!obj.css('position') || obj.css('position') == 'static'){
+                    obj.css({
+                        position: 'relative'
+                    });
+                }
+                
+                var box_width = obj.width(),
+                    box_height = obj.height(),
+                    parent_scrollTop = obj.scrollTop(),
+                    parent_scrollLeft = obj.scrollLeft();
+                
+                if(obj.is("body")){
+                    box_width = $("body").width(),
+                    box_height = $(window).height()-10,
+                    parent_scrollTop = $(window).scrollTop(),
+                    parent_scrollLeft = $(window).scrollLeft();
+                 }
+                $(".screw-hit-test-box").show();
+                $(".screw-hit-test-box").css({
+                    'width': box_width+'px',
+                    'min-width': box_width+'px',
+                    'max-width': box_width+'px',
+                    'height': box_height+'px',
+                    'min-height': box_height+'px',
+                    'max-height': box_height+'px',
+                    'position': 'absolute',
+                    'top': parent_scrollTop+'px',
+                    'left': parent_scrollLeft+'px',
+                    'padding': '0px',
+                    'margin': '0px',
+                    'border': '0px solid red'
+                });
+                /**/
+                $(".screw-image").each(function(){ 
+                    var o = $(this),
+                        mother = $(".screw-hit-test-box"), 
+                        rand = Math.round(Math.random()*1000), 
+                        data = o.data();
                         
-                        $(this).addClass('screw-loading').html('<div id="screw-loading-' + rand + '">' + option.loadingHTML + '</div>');
+                    if(is_viewable(mother, o)){
+                        
+                    if(!o.hasClass('screw-loaded') && !o.hasClass('screw-loading')){
+                        
+                        if(data.debug) try{console.log("Screw loading");}catch(e){}
+                        o.addClass('screw-loading').html('<div id="screw-loading-' + rand + '">' + option.loadingHTML + '</div>');
                         // Stop cache
-                        var url = $(this).attr('rel'), patt = /&/g;
+                        var url = o.attr('rel'), patt = /&/g;
                         if(patt.test(url)){
                             url += '&screw_rand=' + rand;
                         } else {
                             url += '?screw_rand=' + rand;
                         }
                         // Preload image
+                        if(data.debug) try{console.log("Preloading image");}catch(e){}
                         objImage = new Image();
                         objImage.src = url;
-                        var o = $(this);
                         objImage.onload = function(){
-                            o.append('<img style="display:none" id="screw-content-' + rand + '" class="screw-content" src="' + url + '" />');                            
+                            o.append('<img style="display:none" id="screw-content-' + rand + '" class="screw-content" src="' + url + '" />');
+                            if(data.debug) try{console.log("Preloading complete");}catch(e){}
                             $('#screw-loading-' + rand).fadeOut('slow', function(){
                                 $('#screw-content-' + rand).fadeIn('slow');
                                 o.removeClass('screw-loading').addClass('screw-loaded');
+                                if(data.debug) try{console.log("Image loaded successfully");}catch(e){}
                             });
                         };
                     }                        
@@ -64,110 +129,132 @@
                 });	
                 
                 $(".screw").each(function(){
-                    var pos = $(this).offset(), o = $(this), rand = Math.round(Math.random()*1000), data_type = 'html';
-                    if(t >= pos.top && pos.left <= w){
-                        if($(this).hasClass('screw-xml')){
+                    var o = $(this),
+                        mother = $(".screw-hit-test-box"), 
+                        rand = Math.round(Math.random()*1000), 
+                        data = o.data(), 
+                        data_type = 'html';
+                    
+                    if(is_viewable(mother, o)){
+                        if(o.hasClass('screw-xml')){
                           data_type = 'xml';  
-                        } else if($(this).hasClass('screw-json')){
+                        } else if(o.hasClass('screw-json')){
                           data_type = 'json';  
-                        } else if($(this).hasClass('screw-script')){
+                        } else if(o.hasClass('screw-script')){
                           data_type = 'script';  
                         }
-                    if((!$(this).hasClass('screw-loaded') || $(this).hasClass('screw-repeat') && !$(this).hasClass('screw-loading'))){
+                        if(data.debug) try{console.log("Data type: " + data_type);}catch(e){}
+                    if((!o.hasClass('screw-loaded') || o.hasClass('screw-repeat') && !o.hasClass('screw-loading'))){
+                        if(data.debug) try{console.log("Screw loading");}catch(e){}
                         o.addClass('screw-loading');
                         if(option.loadingHTML){
+                            if(data.debug) try{console.log("Applying loading html");}catch(e){}
                             o.html('<div id="screw-loading-' + rand + '">' + option.loadingHTML + '</div>');
                         }
                         
                         if(o.hasClass('screw-replace')){
-                        if($(this).attr('rel')){
-                          $.get($(this).attr('rel'), { screwrand : Math.round(Math.random()*1000) }, function(data) {
+                            if(data.debug) try{console.log("Screw replace");}catch(e){}
+                        if(o.attr('rel')){
+                          $.get(o.attr('rel'), { screwrand : Math.round(Math.random()*1000) }, function(data) {
                              o.replaceWith(data);
                         }, data_type);
-                        } else if($(this).attr('rev')){
-                            o.replaceWith($(this).attr('rev'));
+                        } else if(o.attr('rev')){
+                            o.replaceWith(o.attr('rev'));
                         }
                         } else if(o.hasClass('screw-append')){
-                        if($(this).attr('rel')){
-                          $.get($(this).attr('rel'), { screwrand : Math.round(Math.random()*1000) }, function(data) {
+                            if(data.debug) try{console.log("Screw append");}catch(e){}
+                        if(o.attr('rel')){
+                          $.get(o.attr('rel'), { screwrand : Math.round(Math.random()*1000) }, function(data) {
                              o.append('<div style="display:none" id="screw-content-' + rand + '" class="screw-content">' + data + '</div>');
                              showContent(rand);
                         }, data_type);  
-                        } else if($(this).attr('rev')){
-                            o.append('<div style="display:none" id="screw-content-' + rand + '" class="screw-content">' + $(this).attr('rev') + '</div>');
+                        } else if(o.attr('rev')){
+                            o.append('<div style="display:none" id="screw-content-' + rand + '" class="screw-content">' + o.attr('rev') + '</div>');
                             showContent(rand);
                         } 
                         } else if(o.hasClass('screw-prepend')){
-                        if($(this).attr('rel')){
-                          $.get($(this).attr('rel'), { screwrand : Math.round(Math.random()*1000) }, function(data) {
+                            if(data.debug) try{console.log("Screw prepend");}catch(e){}
+                        if(o.attr('rel')){
+                          $.get(o.attr('rel'), { screwrand : Math.round(Math.random()*1000) }, function(data) {
                              o.prepend('<div style="display:none" id="screw-content-' + rand + '" class="screw-content">' + data + '</div>');
                              showContent(rand);
                         }, data_type);  
-                        } else if($(this).attr('rev')){
-                            o.prepend('<div style="display:none" id="screw-content-' + rand + '" class="screw-content">' + $(this).attr('rev') + '</div>');
+                        } else if(o.attr('rev')){
+                            o.prepend('<div style="display:none" id="screw-content-' + rand + '" class="screw-content">' + o.attr('rev') + '</div>');
                             showContent(rand);
                         } 
                         } else if(o.hasClass('screw-before')){
-                        if($(this).attr('rel')){
-                          $.get($(this).attr('rel'), { screwrand : Math.round(Math.random()*1000) }, function(data) {
+                            if(data.debug) try{console.log("Screw before");}catch(e){}
+                        if(o.attr('rel')){
+                          $.get(o.attr('rel'), { screwrand : Math.round(Math.random()*1000) }, function(data) {
                              o.before('<div style="display:none" id="screw-content-' + rand + '" class="screw-content">' + data + '</div>');
                              showContent(rand);
                         }, data_type);  
-                        } else if($(this).attr('rev')){
-                            o.before('<div style="display:none" id="screw-content-' + rand + '" class="screw-content">' + $(this).attr('rev') + '</div>');
+                        } else if(o.attr('rev')){
+                            o.before('<div style="display:none" id="screw-content-' + rand + '" class="screw-content">' + o.attr('rev') + '</div>');
                             showContent(rand);
                         }
                         
-                        if(o.hasClass('screw-repeat') && pos.top < $(window).height() && pos.left < $(window).width()){
-                            if($(this).attr('rel')){
-                            $.get($(this).attr('rel'), { screwrand : Math.round(Math.random()*1000) }, function(data) {
+                        if(o.hasClass('screw-repeat') && t >= Math.round(os.top) && true_left >= Math.round(os.left)){
+                            if(data.debug) try{console.log("Screw repeat");}catch(e){}
+                            if(o.attr('rel')){
+                            $.get(o.attr('rel'), { screwrand : Math.round(Math.random()*1000) }, function(data) {
                                     o.before('<div style="display:none" id="screw-content-' + rand + '" class="screw-content">' + data + '</div>');
                                     showContent(rand);
                             }, data_type);
-                            } else if($(this).attr('rev')){
-                                o.before('<div style="display:none" id="screw-content-' + rand + '" class="screw-content">' + $(this).attr('rev') + '</div>');
+                            } else if(o.attr('rev')){
+                                o.before('<div style="display:none" id="screw-content-' + rand + '" class="screw-content">' + o.attr('rev') + '</div>');
                                 showContent(rand);
                             }
                         }
                          
                         } else if(o.hasClass('screw-after')){
-                        if($(this).attr('rel')){
-                          $.get($(this).attr('rel'), { screwrand : Math.round(Math.random()*1000) }, function(data) {
+                            if(data.debug) try{console.log("Screw after");}catch(e){}
+                        if(o.attr('rel')){
+                          $.get(o.attr('rel'), { screwrand : Math.round(Math.random()*1000) }, function(data) {
                              o.after('<div style="display:none" id="screw-content-' + rand + '" class="screw-content">' + data + '</div>');
                              showContent(rand);
                         }, data_type);  
-                        } else if($(this).attr('rev')){
-                            o.after('<div style="display:none" id="screw-content-' + rand + '" class="screw-content">' + $(this).attr('rev') + '</div>');
+                        } else if(o.attr('rev')){
+                            o.after('<div style="display:none" id="screw-content-' + rand + '" class="screw-content">' + o.attr('rev') + '</div>');
                             showContent(rand);
                         } 
                         } else {
-                        if($(this).attr('rel')){
-                          $.get($(this).attr('rel'), { screwrand : Math.round(Math.random()*1000) }, function(data) {
+                        if(o.attr('rel')){
+                          $.get(o.attr('rel'), { screwrand : Math.round(Math.random()*1000) }, function(data) {
                              o.append('<div style="display:none" id="screw-content-' + rand + '" class="screw-content">' + data + '</div>');
                              showContent(rand);
                         }, data_type);  
-                        } else if($(this).attr('rev')){
-                            o.append('<div style="display:none" id="screw-content-' + rand + '" class="screw-content">' + $(this).attr('rev') + '</div>');
+                        } else if(o.attr('rev')){
+                            o.append('<div style="display:none" id="screw-content-' + rand + '" class="screw-content">' + o.attr('rev') + '</div>');
                             showContent(rand);
                         } 
                         }
                         o.removeClass('screw-loading').addClass('screw-loaded');
+                        if(data.debug) try{console.log("Screw loaded successfully");}catch(e){}
                     }                        
                     }
                 });
                 
                 $(".screw-remove").each(function(){
-                    if($(this).hasClass('screw-loaded')){
-                        var p = $(this).position();
-                        if(p.top < st || p.left > w){
-                            if($(this).is(':visible')){
-                                $(this).fadeOut('slow');
+                    var o = $(this),
+                        mother = $(".screw-hit-test-box"), 
+                        rand = Math.round(Math.random()*1000), 
+                        data = o.data();
+                    if(o.hasClass('screw-loaded') && is_viewable(mother, o)){                       
+                        if(data.debug) try{console.log("Has Class screw-remove.");}catch(e){}
+                        if(o.is(':visible')){
+                                o.fadeOut('slow');
+                                o.removeClass('screw-loaded');
+                                if(data.debug) try{console.log("Screw removed successfully");}catch(e){}
                             }
-                        }
                     }
                 });
                 
-              };
+                $(".screw-hit-test-box").hide();
+                
+              }; // screwIt
+              
               
               var showContent = function(rand){
                 if(option.loadingHTML){
@@ -177,12 +264,31 @@
                 } else {
                     $('#screw-content-' + rand).fadeIn('slow');
                 }               
-              };
+              }; // showContent
               
-              screwIt($(window));
-    		});
-    	}
-	});
+              if(!$(".screw-hit-test-box").width()){
+                obj.prepend('<div class="screw-hit-test-box"></div>');
+              }
+              
+              if(obj.is("body")){
+                $(window).scroll(function() {
+                 screwIt();                             
+                }).resize(function() {
+                 screwIt();                             
+                });
+                } else {
+                obj.scroll(function() {
+                 screwIt();                             
+                }).resize(function() {
+                 screwIt();                             
+                });
+              }
+              
+              screwIt(); // init               
+                
+    		}); // each
+    	} // screw
+	}); // extend
 	
 })(jQuery);
  -->
